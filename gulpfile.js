c56f62e9b63b3aab9953cc40b13ff726a4bc6db3
Jannik Zschiesche <jz@becklyn.com>
2014-08-07 15:21:13 +0200

    Added generic link task for content

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var isDebug = !!require("yargs").argv.debug || !!require("yargs").argv.dev;
 gulp.task("css", becklyn.scss("layout/assets/**/*.scss", isDebug));
 gulp.task("js",  becklyn.js("layout/assets/js/**/*.js", isDebug));
 
-gulp.task("assets", ["css", "js"],
+gulp.task("link", ["css", "js"],
     function ()
     {
         if (fs.existsSync("web/assets"))
@@ -14,6 +14,14 @@ gulp.task("assets", ["css", "js"],
         }
 
         fs.symlinkSync("../layout/public", "web/assets");
+
+
+        if (fs.existsSync("web/content"))
+        {
+            fs.unlinkSync("web/content");
+        }
+
+        fs.symlinkSync("../layout/content", "web/content");
     }
 );
 
